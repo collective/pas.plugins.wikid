0fb2cc81d4dd44f6b591734f251c0346737f97bd
Taras Poburynnyi <potar@quintagroup.com>
2013-08-12 12:39:02 +0300

    remove unnecessary code

--- pas/plugins/wikid/pywClient.py
+++ pas/plugins/wikid/pywClient.py
@@ -4,11 +4,7 @@ Python Client for wikid '''
 from OpenSSL import *
 from xml.dom import minidom, Node
 import sys
-import re
-import select
 import socket
-import os
-import traceback
 import time
 
 __author__ = "Manish Rai Jain <manishrjain@gmail.com>"
@@ -29,7 +25,7 @@ def verify_cb(conn, cert, errnum, depth, ok):
 
 class pywClient:
 
-    def __init__(self, filename=None, host=None, port=None, pkey=None, passPhrase=None, caCert=None):
+    def __init__(self, host=None, port=None, pkey=None, passPhrase=None, caCert=None):
         """ This will create a SSL Connection b/w client and server.
         :param host: IP address of WIKID server
         :type host: string
@@ -45,18 +41,12 @@ class pywClient:
         """
 
         DEBUG("DEBUG activated")
-        self.filename = filename
         self.host = host
         self.port = port
         self.pkey = pkey
         self.passPhrase = passPhrase
         self.cacert = caCert
 
-        if filename is not None:
-            # Parse the properties file here
-            print ''
-
-        #ctx = SSL.Context(SSL.TLSv1_METHOD)
         ctx = SSL.Context(SSL.SSLv3_METHOD)
         ctx.set_verify(SSL.VERIFY_PEER, verify_cb)
         ctx.set_verify(SSL.VERIFY_NONE, verify_cb)
@@ -73,8 +63,6 @@ class pywClient:
         ctx.use_privatekey(pkeyObj)
         ctx.use_certificate(x509Obj)
 
-        #ctx.load_verify_locations(ca_cert)
-
         self.sock = SSL.Connection(
             ctx, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
         self.gotConnection = False
@@ -88,7 +76,6 @@ class pywClient:
         message = message.replace('\n', '')
         message = message.replace('\r', '')
         message = message + "\n"
-        #message = '<?xml version="1.0" encoding="UTF-8"?>\n' + message
         response = self.request(message)
         DEBUG("XML response:")
         DEBUG("--------------------------------")
@@ -344,30 +331,3 @@ class pywClient:
         response = self.xmlrequest(message)
         result = response.getElementsByTagName("domain-list")[0]
         print result
-
-
-# The testing purposes.
-# You should set up input parameters (host, username, etc.) for this test.
-# So, before executing this example you have to:
-#  1) Install one of the token clients
-#  (http://wikidsystems.com/downloads/token-clients)
-#  2) Run your clients and push "Create New Domain",
-#  enter "Server Code" and PIN. Please, remember a PIN because
-#  it's you 'regcode' (see the method -'registerUsername' for details).
-#  Push "Get Passcode" and you will get 'passcode' (see the method - 'checkCredentials' for details)
-#  3) Fill in other parameters.
-if __name__ == "__main__":
-    w = pywClient(host='127.0.0.1', port=8388, pkey='localhost.p12',
-                  passPhrase='secret', caCert='WiKID-ca.pem')
-    w.startConnection()
-    # You will not verify your user if it's not registered.
-    # That's why you can find below the method - 'registerUsername'
-    # for this purpose. You should notice that values of parameters
-    # in methods 'checkCredentials' and registerUsername are the same.
-    if w.checkCredentials(user='testuser', domaincode='127000000001', passcode='passcode'):
-        print 'Cool Valid user'
-    else:
-        print 'No lah! No Entry!'
-        # fill in 'regcode' which you get from a token client.
-        print w.registerUsername(
-            uname='testuser', regcode='regcode', domaincode='127000000001')
