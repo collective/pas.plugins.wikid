a2e530f1bfe4a9f88857b3282a58a18875794503
Taras Poburynnyi <potar@quintagroup.com>
2013-08-22 15:37:25 +0300

    rewrite using of WikidClient

--- pas/plugins/wikid/example.py
+++ pas/plugins/wikid/example.py
@@ -1,4 +1,5 @@
 """ It's dedicated to testing purposes """
+
 from .client import WikidClient
 
 # You should set up input parameters (host, username, etc.) for this test.
@@ -8,19 +9,18 @@ from .client import WikidClient
 #  2) Run your clients and push "Create New Domain",
 #  enter "Server Code" and PIN. Please, remember a PIN because
 #  it's you 'regcode' (see the method -'registerUsername' for details).
-#  Push "Get Passcode" and you will get 'passcode' (see the method - 'checkCredentials' for details)
+#  Push "Get Passcode" and you will get 'passcode' (see the method - 'login' for details)
 #  3) Fill in other parameters.
 w = WikidClient(host='127.0.0.1', port=8388, pkey='localhost.p12',
-              passPhrase='secret', caCert='WiKID-ca.pem')
-w.startConnection()
+                pass_phrase='secret', cacert='WiKID-ca.pem')
+w.setUpConnection()
 # You will not verify your user if it's not registered.
 # That's why you can find below the method - 'registerUsername'
 # for this purpose. You should notice that values of parameters
-# in methods 'checkCredentials' and registerUsername are the same.
-if w.checkCredentials(user='testuser', domaincode='127000000001', passcode='passcode'):
+# in methods 'login' and registerUsername are almost the same.
+if w.login('testuser', 'passcode', '127000000001'):
     print 'Cool Valid user'
 else:
     print 'No lah! No Entry!'
     # fill in 'regcode' which you get from a token client.
-    print w.registerUsername(
-        uname='testuser', regcode='regcode', domaincode='127000000001')
+    print w.registerUsername('testuser', 'regcode', '127000000001')
