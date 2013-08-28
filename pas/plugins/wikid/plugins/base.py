06f08d136867e98ecb5d8d524e9d2f0d9920500a
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2013-08-28 14:59:18 +0300

    added base wikid plugin

---
+++ pas/plugins/wikid/plugins/base.py
@@ -0,0 +1,45 @@
+import os
+from Globals import InitializeClass
+from OFS.Cache import Cacheable
+from AccessControl import ClassSecurityInfo
+from AccessControl.requestmethod import postonly
+
+from Products.CMFCore.permissions import ManagePortal
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
+
+from pas.plugins.wikid.client import WikidClient
+
+
+class WiKIDBasePlugin(BasePlugin, Cacheable):
+
+    meta_type = 'WiKIDBasePlugin'
+    security = ClassSecurityInfo()
+
+    security.declarePrivate('_getWikidConnection')
+
+    def __init__(self, id, title=None):
+        self._id = self.id = id
+        self.title = title
+        self.wikid_port = 8388
+        self.wikid_host = "127.0.0.1"
+        self.domaincode = '127000000001'
+        self.passPhrase = 'passphrase'
+        self.caCert = ''
+        self.pkey = ''
+        self.connector = None
+
+    def _getWikidConnection(self):
+        if not self.connector:
+            try:
+                self.connector = WikidClient(host=self.wikid_host,
+                                             port=self.wikid_port,
+                                             pkey=self.pkey,
+                                             pass_phrase=self.passPhrase,
+                                             cacert=self.caCert)
+                self.connector.setUpConnection()
+            except:
+                return None
+        return self.connector
+
+InitializeClass(WiKIDBasePlugin)
