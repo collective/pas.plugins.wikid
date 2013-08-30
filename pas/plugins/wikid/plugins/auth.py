68c7bc39b3ef7b186c662f80d1e78d9e9cb03b9b
Taras Poburynnyi <potar@quintagroup.com>
2013-08-30 11:52:36 +0300

    rewrite AuthPlugin

--- pas/plugins/wikid/plugins/auth.py
+++ pas/plugins/wikid/plugins/auth.py
@@ -1,27 +1,38 @@
 import logging
 from AccessControl import ClassSecurityInfo
 from zope.interface import implements
-from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
+from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin, IExtractionPlugin
 
 logger = logging.getLogger("pas.plugins.wikid.auth")
 
 
+# TODO: rewrite it
 class AuthPlugin(object):
-    """ Implements IAuthenticationPlugin
+    """ It's a mixin for WiKIDAuthMultiPlugin
     """
-    implements(IAuthenticationPlugin)
+    implements(IAuthenticationPlugin, IExtractionPlugin)
     meta_type = 'WiKID Auth Plugin'
     security = ClassSecurityInfo()
+
+    security.declarePrivate('extractCredentials')
+
+    def extractCredentials(self, request):
+        if '__ac_name' in request and '__ac_password' in request:
+            return {
+                'src': self.getId(),
+                'login': request.get('__ac_name'),
+                'password': request.get('__ac_password')
+            }
 
     security.declarePrivate('authenticateCredentials')
 
     def authenticateCredentials(self, credentials):
-
-        """ See IAuthenticationPlugin.
-
-        o We expect the credentials to be those returned by
-          ILoginPasswordExtractionPlugin.
+        """ We expect that credentials will be returned by
+          extractCredentials.
         """
+        if credentials.get('src') != self.getId():
+            return
+
         login = credentials.get('login')
         password = credentials.get('password')
 
@@ -29,8 +40,8 @@ class AuthPlugin(object):
             return None
         # check credentials using a wikid server
         connector = self._getWikidConnection()
-        res = connector.login(login, password, self.domaincode)
-        if res is True:
+        result = connector.login(login, password, self.domaincode)
+        if result is True:
             return login, login
         else:
             print None
