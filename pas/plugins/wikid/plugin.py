86cdd4b85f2fa90ce50558e52c6694ba35aa7abf
Taras Poburynnyi <potar@quintagroup.com>
2013-09-02 17:31:16 +0300

    reorganize plugins (refactoring)

--- pas/plugins/wikid/plugin.py
+++ pas/plugins/wikid/plugin.py
@@ -13,14 +13,8 @@ info at wikidsystems.com
 866-244-1876
 """
 
-import os
 from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
-from AccessControl.requestmethod import postonly
-
-from Products.CMFCore.permissions import ManagePortal
-from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 
 from pas.plugins.wikid.client import WikidClient
 from pas.plugins.wikid.plugins.auth import AuthPlugin
@@ -36,61 +30,20 @@ class WiKIDAuthMultiPlugin(WiKIDBasePlugin, AuthPlugin, UserEnumerator):
     meta_type = 'WiKID Auth Multi Plugin'
 
     security = ClassSecurityInfo()
+    security.declarePrivate('_getWikidConnection')
 
-    # ZMI tab for configuration page
-    manage_options = (({'label': 'Configuration',
-                        'action': 'manage_config'},)
-                      + BasePlugin.manage_options)
+    def _getWikidConnection(self):
+        # '_v_connector' will not be persisted due to '_v_'
+        if not hasattr(self, '_v_connector'):
+            self._v_connector = WikidClient(
+                host=self.wikid_host,
+                port=self.wikid_port,
+                pkey=self.pkey,
+                pass_phrase=self.passPhrase,
+                cacert=self.caCert
+            )
+            self._v_connector.setUpConnection()
+        return self._v_connector
 
-    security.declareProtected(ManagePortal, 'manage_config')
-    manage_config = PageTemplateFile('www/config', globals(),
-                                     __name__='manage_config')
-
-    security.declareProtected(ManagePortal, 'manage_updateConfig')
-
-    @postonly
-    def manage_updateConfig(self, REQUEST):
-        """Update configuration of Trusted Proxy Authentication Plugin.
-        """
-        def verify():
-            msg = "Configuration Error: "
-
-            try:
-                int(wikid_port)
-            except ValueError:
-                return msg + "  'Port' must be an integer."
-            if not os.path.exists(pkey):
-                return msg + " Cannot access to '%s' No such file." % pkey
-            if not os.path.exists(caCert):
-                return msg + " Cannot access to '%s' No such file." % caCert
-            try:
-                WikidClient(host=wikid_host, port=wikid_port, pkey=pkey,
-                            pass_phrase=passPhrase, cacert=caCert)
-            except:
-                return msg + " WIKID Client error. Check certificates."
-
-        response = REQUEST.response
-        wikid_port = REQUEST.form.get('wikid_port')
-        wikid_host = REQUEST.form.get('wikid_host')
-        domaincode = REQUEST.form.get('domaincode')
-        passPhrase = REQUEST.form.get('passPhrase')
-        caCert = REQUEST.form.get('caCert')
-        pkey = REQUEST.form.get('pkey')
-
-        err = verify()
-        if not err:
-            self.wikid_port = int(wikid_port)
-            self.wikid_host = wikid_host
-            self.domaincode = domaincode
-            self.passPhrase = passPhrase
-            self.caCert = caCert
-            self.pkey = pkey
-
-            response.redirect('%s/manage_config?manage_tabs_message=%s' %
-                              (self.absolute_url(), 'Configuration+updated.'))
-        else:
-            response.redirect('%s/manage_config?manage_tabs_message=%s' %
-                              (self.absolute_url(),
-                               err + ' Configuration+NOT+updated.'))
 
 InitializeClass(WiKIDAuthMultiPlugin)
--- pas/plugins/wikid/plugins/base.py
+++ pas/plugins/wikid/plugins/base.py
@@ -1,5 +1,10 @@
+import os
+
 from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
+from AccessControl.requestmethod import postonly
+from Products.CMFCore.permissions import ManagePortal
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 
 from pas.plugins.wikid.client import WikidClient
@@ -9,7 +14,17 @@ class WiKIDBasePlugin(BasePlugin):
     """ It's a mixin for WiKIDAuthMultiPlugin
     """
     meta_type = 'WiKIDBasePlugin'
+
     security = ClassSecurityInfo()
+
+    # ZMI tab for configuration page
+    manage_options = (({'label': 'Configuration',
+                        'action': 'manage_config'},)
+                      + BasePlugin.manage_options)
+
+    security.declareProtected(ManagePortal, 'manage_config')
+    manage_config = PageTemplateFile('../www/config', globals(),
+                                     __name__='manage_config')
 
     def __init__(self, id, title=None):
         self._id = self.id = id
@@ -21,19 +36,51 @@ class WiKIDBasePlugin(BasePlugin):
         self.caCert = ''
         self.pkey = ''
 
-    security.declarePrivate('_getWikidConnection')
+    security.declareProtected(ManagePortal, 'manage_updateConfig')
 
-    def _getWikidConnection(self):
-        # '_v_connector' will not be persisted due to '_v_'
-        if not hasattr(self, '_v_connector'):
-            self._v_connector = WikidClient(
-                host=self.wikid_host,
-                port=self.wikid_port,
-                pkey=self.pkey,
-                pass_phrase=self.passPhrase,
-                cacert=self.caCert
-            )
-            self._v_connector.setUpConnection()
-        return self._v_connector
+    @postonly
+    def manage_updateConfig(self, REQUEST):
+        """Update configuration of Trusted Proxy Authentication Plugin.
+        """
+        def verify():
+            msg = "Configuration Error: "
+
+            try:
+                int(wikid_port)
+            except ValueError:
+                return msg + "  'Port' must be an integer."
+            if not os.path.exists(pkey):
+                return msg + " Cannot access to '%s' No such file." % pkey
+            if not os.path.exists(caCert):
+                return msg + " Cannot access to '%s' No such file." % caCert
+            try:
+                WikidClient(host=wikid_host, port=wikid_port, pkey=pkey,
+                            pass_phrase=passPhrase, cacert=caCert)
+            except:
+                return msg + " WIKID Client error. Check certificates."
+
+        response = REQUEST.response
+        wikid_port = REQUEST.form.get('wikid_port')
+        wikid_host = REQUEST.form.get('wikid_host')
+        domaincode = REQUEST.form.get('domaincode')
+        passPhrase = REQUEST.form.get('passPhrase')
+        caCert = REQUEST.form.get('caCert')
+        pkey = REQUEST.form.get('pkey')
+
+        err = verify()
+        if not err:
+            self.wikid_port = int(wikid_port)
+            self.wikid_host = wikid_host
+            self.domaincode = domaincode
+            self.passPhrase = passPhrase
+            self.caCert = caCert
+            self.pkey = pkey
+
+            response.redirect('%s/manage_config?manage_tabs_message=%s' %
+                              (self.absolute_url(), 'Configuration+updated.'))
+        else:
+            response.redirect('%s/manage_config?manage_tabs_message=%s' %
+                              (self.absolute_url(),
+                               err + ' Configuration+NOT+updated.'))
 
 InitializeClass(WiKIDBasePlugin)
