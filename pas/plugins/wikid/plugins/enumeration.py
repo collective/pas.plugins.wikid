b0ab6eec7ff0dfd0e72139d75aec332b35b03f9a
Taras Poburynnyi <potar@quintagroup.com>
2013-09-04 13:21:55 +0300

    add cache for enumerateUsers

--- pas/plugins/wikid/plugins/enumeration.py
+++ pas/plugins/wikid/plugins/enumeration.py
@@ -1,9 +1,11 @@
 """ It's dedicated to enumeration plugins """
+import copy
 from itertools import ifilter
 
 from zope.interface import implements
 from AccessControl import ClassSecurityInfo
 from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin
+from Products.PluggableAuthService.utils import createViewName
 
 
 class UserEnumerator(object):
@@ -15,8 +17,8 @@ class UserEnumerator(object):
 
     security.declarePrivate('enumerateUsers')
 
-    def enumerateUsers(self, id=None, login=None, exact_match=False,
-                       sort_by=None, max_results=None, **kw):
+    def getUsers(self):
+        """ Get users from a wikid server """
         connector = self._getWikidConnection()
         users = (user for user in connector.listUsers(self.domaincode))
         info = lambda user: {
@@ -24,8 +26,42 @@ class UserEnumerator(object):
             'login': user,
             'pluginid': self.getId()
         }
-        result = (info(user) for user in users)
+        return (info(user) for user in users)
+
+    def enumerateUsers(self, id=None, login=None, exact_match=False,
+                       sort_by=None, max_results=None, **kw):
+        # we don't support search by e-mail and fullname for now
+        if 'email' in kw or 'fullname' in kw:
+            return ()
+
+        username = login or id
+
+        def getRAMKeys():
+            keywords = copy.deepcopy(kw)
+            info = {
+                'plugin_id': self.getId(),
+                'id_or_login': username,
+                'sort_by': sort_by,
+                'exact_match': exact_match,
+                'max_results': max_results,
+            }
+            keywords.update(info)
+            return keywords
+
+        # Check cached data
+        view_name = createViewName('enumerateUsers')
+        keys = getRAMKeys()
+        user_info = self.ZCacheable_get(
+            view_name=view_name,
+            keywords=keys,
+        )
+        if user_info is not None:
+            return user_info
+        # get user fields
+        user_fields = self.getUsers()
         if exact_match:
-            key = login or id
-            result = ifilter(lambda user: key == user.get('id'), result)
-        return tuple(result)
+            user_fields = ifilter(lambda user: username == user.get('id'), user_fields)
+        # Cache data upon success
+        retvalues = list(user_fields)
+        self.ZCacheable_set(retvalues, view_name=view_name, keywords=keys)
+        return retvalues
